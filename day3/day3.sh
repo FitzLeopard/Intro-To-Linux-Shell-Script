55195d35062c7ca679b1a0ec97f7249797774a1c
FitzLeopold <1796229518@qq.com>
2018-05-29 19:07:27 +0800

    [+] Day 3 source code

---
+++ day3/day3.sh
@@ -0,0 +1,11 @@
+#!/bin/bash
+
+# This script is used for variable practice
+
+# Initialize a variable
+WORD="script"
+echo "${WORD}ing is fun!"
+
+# Change the value of the variable(Reassignment).
+WORD="read"
+echo "${WORD}ing is fun!"
